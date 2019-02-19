9e1e837da13a619d6e3a8623cd34e369402b6395
somberSomni <kartunehustla@gmail.com>
2019-02-19 08:42:29 -0600

    new app started

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 80;
+let savedColor = '255,0,0'; //start red
+
+function parseColor(color) {
+    const noHash = color.slice(1, color.length -1);
+    let newColor = '';
+    if (noHash.length == 6) {
+        for (let i = 0; i < 6; i+=2) {
+            let eachColor = noHash.slice(i, i + 2);
+            eachColor += i != 4 ? ',' : '';
+            newColor += eachColor;
+        }
+    }
+    return newColor;
+}
+app.use(bodyParser.text());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.render("public");
+});
+app.post("/setcolor", (req, res) => {
+    savedColor = parseColor(req.body);
+    res.send(`Set color to rgb(${savedColor})`);
+    res.end();
+});
+app.get("/getcolor", (req, res) => {
+    res.send(savedColor);
+    res.end();
+});
+app.listen(PORT, () => {
+    console.log("Server on port " + PORT);
+})
