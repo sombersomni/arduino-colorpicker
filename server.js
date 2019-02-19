93f6fdfddc3b66f29a7190d4649177a2885e605e
somberSomni <kartunehustla@gmail.com>
2019-02-19 09:49:57 -0600

    add last changed header

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ if (port == null || port == "") {
   port = 8000;
 }
 let savedColor = '255,0,0'; //start red
-
+let lastChanged = setTime(Date.now());
+console.log(lastChanged);
+function setTime(time) {
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
+    const date = new Date(time);
+    const clock =  date.toLocaleTimeString('it-IT');
+    return days[date.getDay()] + ', ' + date.getDate() + ' ' + 
+    months[date.getMonth()] + ' ' + date.getFullYear() + ' ' + 
+    clock.slice(0, clock.length - 3) + ' GMT';
+}
 function parseColor(color) {
     const noHash = color.slice(1, color.length -1);
     let newColor = '';
@@ -26,11 +36,13 @@ app.get("/", (req, res) => {
     res.render("public");
 });
 app.post("/setcolor", (req, res) => {
+    lastChanged = setTime(Date.now());
     savedColor = parseColor(req.body);
     res.send(`Set color to rgb(${savedColor})`);
     res.end();
 });
 app.get("/getcolor", (req, res) => {
+    res.set("Last-Modified", lastChanged);
     res.send(savedColor);
 });
 app.listen(port, () => {
